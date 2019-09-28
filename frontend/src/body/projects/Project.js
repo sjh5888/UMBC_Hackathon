395af2a3cde0ee91d8a099e1e43147f9109f4889
Quinnan Gill <quinnan.gill@gmail.com>
2019-09-28 19:31:26 -0400

    May break

--- frontend/src/body/projects/Project.js
+++ frontend/src/body/projects/Project.js
@@ -1,11 +1,27 @@
 import React from 'react';
 
-function Project() {
-  return (
-    <div>
-      Projects
-    </div>
-  );
+// function Project() {
+//   return (
+//     <div>
+//       Projects
+//     </div>
+//   );
+// }
+
+class Project extends React.Component {
+  constructor(props) {
+    super();
+
+    this.state = {
+      projectId: null
+    }
+  }
+
+  componentDidMount() {
+    const { handle } = this.props.match.params;
+
+    this 
+  }
 }
 
 export default Project;
--- frontend/src/data/hackathon_data.js
+++ frontend/src/data/hackathon_data.js
@@ -3,19 +3,32 @@ export const hackathons = [
     "title": "Hack UMBC",
     "image": "https://s3.amazonaws.com/assets.mlh.io/events/logos/000/001/219/thumb/HackUMBC.Facebook.Logo_%281%29.jpg?1564062845",
     "attendants": [
-      "Jack Smith"
+      "Jack Smith",
+      "George Holtz"
     ]
   },
   {
     "title": "Hack UMBC",
     "image": "https://s3.amazonaws.com/assets.mlh.io/events/logos/000/001/219/thumb/HackUMBC.Facebook.Logo_%281%29.jpg?1564062845",
+    "attendants": [
+      "Jack Smith",
+      "George Holtz"
+    ]
   },
   {
     "title": "Hack UMBC",
     "image": "https://s3.amazonaws.com/assets.mlh.io/events/logos/000/001/219/thumb/HackUMBC.Facebook.Logo_%281%29.jpg?1564062845",
+    "attendants": [
+      "Jack Smith",
+      "George Holtz"
+    ]
   },
   {
     "title": "Hack UMBC",
     "image": "https://s3.amazonaws.com/assets.mlh.io/events/logos/000/001/219/thumb/HackUMBC.Facebook.Logo_%281%29.jpg?1564062845",
+    "attendants": [
+      "Jack Smith",
+      "George Holtz"
+    ]
   },
 ]
