7b05702e76a03039aabc7d52bb7a09f57f815e69
Quinnan Gill <quinnan.gill@gmail.com>
2019-09-28 16:21:49 -0400

    Added dummy data

---
+++ frontend/src/data/hackathon_data.js
@@ -0,0 +1,18 @@
+export const hackathons = [
+  {
+    "title": "Hack UMBC",
+    "image": "https://s3.amazonaws.com/assets.mlh.io/events/logos/000/001/219/thumb/HackUMBC.Facebook.Logo_%281%29.jpg?1564062845",
+  },
+  {
+    "title": "Hack UMBC",
+    "image": "https://s3.amazonaws.com/assets.mlh.io/events/logos/000/001/219/thumb/HackUMBC.Facebook.Logo_%281%29.jpg?1564062845",
+  },
+  {
+    "title": "Hack UMBC",
+    "image": "https://s3.amazonaws.com/assets.mlh.io/events/logos/000/001/219/thumb/HackUMBC.Facebook.Logo_%281%29.jpg?1564062845",
+  },
+  {
+    "title": "Hack UMBC",
+    "image": "https://s3.amazonaws.com/assets.mlh.io/events/logos/000/001/219/thumb/HackUMBC.Facebook.Logo_%281%29.jpg?1564062845",
+  },
+]
