11c853d2c75bd11d21f075e3d81297b0864f4922
EDR009 <PPPepe123456789@gmail.com>
2019-09-28 23:38:12 -0400

    asdasd

--- backend/src/main/java/com/Hitch/backend/BackendApplication.java
+++ backend/src/main/java/com/Hitch/backend/BackendApplication.java
@@ -2,12 +2,20 @@ package com.Hitch.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
+@RestController
 public class BackendApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
+	@GetMapping("/")
+	  public String hello() {
+	    return "hello world!";
+	  }
 }
